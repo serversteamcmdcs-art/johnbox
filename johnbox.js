90f68329e8b45d1221c103da59e34104b6a29df7
serversteamcmdcs-art <serversteamcmdcs@gmail.com>
2026-03-28 21:15:07 +0500

    Update johnbox.js

--- johnbox.js
+++ johnbox.js
@@ -24,7 +24,7 @@ const util = require('util')
 var URL = require("url");
 
 // the host that the custom server is accessible with
-var accessibleHost = "jackbox.c6t.ru";
+var accessibleHost = "johnbox.onrender.com";
 // the TLS certificate / key used for the server
 // must have valid SNI for accessibleHost
 // var tlsCertificate = "certs/192.168.1.10.pem";
